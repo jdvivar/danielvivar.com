f40aa2c731182ca36fbb4d56d26fc567ba662ffa
Daniel Vivar <jdvivar@gmail.com>
2018-07-14 16:01:55 +0100

    Leave with default appearance

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Content />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Content from "./components/Content.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Content
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
