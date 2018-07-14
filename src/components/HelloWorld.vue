b333cf0b21a22055fd8272da8ef620f6bfc75693
Daniel Vivar <jdvivar@gmail.com>
2018-07-14 06:18:17 +0100

    Change to vue app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,342 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+  <nav>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
+      <li><a href="#who">who I am</a></li>
+      <li><a href="#future">what I'm looking for</a></li>
+      <li><a href="#outstanding">what I'm good at</a></li>
+      <li><a href="#tools">which technologies or tools I use</a></li>
+      <li><a href="#experience">where I've worked</a></li>
+      <li><a href="#education">where I've studied</a></li>
+      <li><a href="#contact">how you can get in touch with me</a></li>
+      <li><a href="#about">about this site</a></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  </nav>
+
+  <main>
+
+    <section id="who">
+      <h1>who I am</h1>
+      <div>
+        <p>
+          Hi, my name is Daniel Vivar and I'm a front-end web engineer and designer based in London since 2014.
+        </p>
+        <p>
+          Originally a graphic designer and a computers engineering student, I then decided to focus my career into front-end web engineering.
+        </p>
+      </div>
+    </section>
+
+    <section id="future">
+      <h1>what I'm looking for</h1>
+      <div>
+        <p>
+          I love to learn new things all the time so my future company should be a place where learning and sharing knowledge is actively encouraged.
+        </p>
+        <p>
+          I feel specially motivated in projects where I could create delightful user experiences for great apps.
+        </p>
+        <p>
+          I also value an open-minded culture, flexibility, a healthy environment, a focused team and companies where technology is the heart.
+        </p>
+      </div>
+    </section>
+
+    <section id="tools">
+      <h1>which technologies or tools I use</h1>
+      <span>javascript</span>
+      <span>es6</span>
+      <span>angularjs</span>
+      <span>bootstrap</span>
+      <span>css</span>
+      <span>sass/scss</span>
+      <span>vuejs</span>
+      <span>tdd</span>
+      <span>bdd</span>
+      <span>soap</span>
+      <span>cro</span>
+      <span>npm</span>
+      <span>node</span>
+      <span>cd/ci</span>
+      <span>git</span>
+      <span>jquery</span>
+      <span>a/b testing</span>
+      <span>e2e testing</span>
+      <span>qa engineering</span>
+      <span>api testing</span>
+      <span>agile</span>
+      <span>scrum</span>
+      <span>kanban</span>
+      <span>prototyping</span>
+      <span>wireframing</span>
+      <span>serverless</span>
+      <span>cloud functions</span>
+      <span>responsive design</span>
+      <span>progressive enhancement</span>
+    </section>
+
+    <section id="outstanding">
+      <h1>what I'm good at</h1>
+      <div>
+        <p>
+          I'm good at abstract thinking: explaining and designing how things work and talking to non-technical people. That means I can easily speak with senior architects, product managers, ux researchers or a CTO and I'll be able to have meaningful relations with each one of them.
+        </p>
+        <p>
+           Having a good synthesis capacity and abstract thinking also helps when designing the architecture of apps and systems and when solving difficult problems and defects.
+        </p>
+        <p>
+          I'm very good at learning new stuff, so when picking up tasks I'll always go for the more challenging ones. That means I also value teams that tend to do pair programming or encourage cross-team work.
+        </p>
+      </div>
+    </section>
+
+    <section id="experience">
+      <h1>where I've worked</h1>
+      <ul>
+        <li class="experience-item">
+          <div class="company">
+            Zava
+          </div>
+          <div class="company-logo">
+            company-logo
+          </div>
+          <div class="title">
+            front-end web engineer
+          </div>
+          <div class="from">
+            2015-10
+          </div>
+          <div class="until">
+            present
+          </div>
+          <div class="location">
+            London
+          </div>
+          <div class="tldr">
+            Almost 3 years as a front-end web engineer in a rapid-growing health company, using the latest technologies
+          </div>
+          <div class="description">
+            <p>
+              In Zava (www.zavamed.com) I was mentored by the head of development from the beginning and had the fantastic opportunity to see a thrilling company grow, re-brand (from DrEd to Zava), move office and many other big events, as well as being part and even leading in some of the things we do.
+            </p>
+            <p>
+              Some of the technologies/skills that I've learnt or developed further while at Zava:
+            </p>
+            <ul>
+              <li>
+                Developing and maintaining several apps in AngularJS 1.5+ and VueJS and mastering javascript on the way.
+              </li>
+              <li>
+                Mastering styling with SASS/SCSS
+              </li>
+              <li>
+                CRO (Conversion Rate Optimisation) which requires several teams working together (Data, UX, Marketing and Engineering). Manage data with Looker and all events with Segment. Creating experiments for A/B testing and more advanced customisation with Optimizely.
+              </li>
+              <li>
+                TDD (test-driven development) writing unit tests with Karma, Jasmine.
+              </li>
+              <li>
+                E2E frontend and API test automation, using Protractor, Jasmine, PhantomJS, WebdriverIO, Frisby.
+              </li>
+              <li>
+                CI/CD (Continuous Integration/Deployment) with Jenkins, Docker, AWS, Github, Netlify.
+              </li>
+              <li>
+                Agile methodologies: Scrum, Kanban.
+              </li>
+              <li>
+                Usign InVision to design high detailed and visually attractive user experiences or just wireframing
+              </li>
+              <li>
+                Went from task runner Gulp and package manager Bower to only Yarn to then everything done with npm scripts and even bash scripts again.
+              </li>
+              <li>
+                Serverless and cloud function technologies with AWS Lambda, Apache OpenWhisk with IBM Cloud Functions
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="experience-item">
+          <div class="company">
+            Typeless
+          </div>
+          <div class="company-logo">
+            company-logo
+          </div>
+          <div class="title">
+            lead ui/ux designer
+          </div>
+          <div class="from">
+            2014-09
+          </div>
+          <div class="until">
+            2015-09
+          </div>
+          <div class="location">
+            London
+          </div>
+          <div class="tldr">
+            UX/UI Designer working on everything from scratch: a real London startup environment
+          </div>
+          <div class="description">
+            <p>
+              Typeless had this idea of being a contacts aggregator (https://www.producthunt.com/posts/typeless). The founders were based in London and a team of developers worked remotely from India. I helped both filling the gap between the 2 teams and getting a complex idea and a set of features intoto a simple and delightful user experience.
+            </p>
+            <p>
+              Part of my work here includes:
+            </p>
+            <ul>
+              <li>
+                A completely responsive layout for the whole app:
+                <a>http://www.danielvivar.com/typeless/topbar.html</a>
+              </li>
+              <li>
+                A design from scratch of an iOS app for iPhone:
+                <a>http://invis.io/G62N9KKQ3</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="experience-item">
+          <div class="company">
+            Atos
+          </div>
+          <div class="company-logo">
+            company-logo
+          </div>
+          <div class="title">
+            hybrid apps developer & researcher - internship
+          </div>
+          <div class="from">
+            2013-10
+          </div>
+          <div class="until">
+            2014-08
+          </div>
+          <div class="location">
+            Madrid
+          </div>
+          <div class="tldr">
+            1 year internship in a big consulting firm as a hybrid apps developer/researcher
+          </div>
+          <div class="description">
+            <p>
+              During this 1-year internship in the Smart Mobility working group within the Solutions Delivery and Architecture department at Atos in the central offices in Madrid, I did develop my very first skills as an engineer. Learnt new technologies, researched a lot, took part in several projects, attended courses and, most of all, soaked in a real business environment.
+            </p>
+            <p>
+              This is a non-comprehensive list of some of the things I did:
+            </p>
+            <ul>
+              <li>
+                Design and development of mobile hybrid apps: using Apache Cordova, common web technologies and some specific light frameworks like jQueryMobile.
+              </li>
+              <li>
+                Automated mobile application testing: using Calabash and Cucumber to create a tests written following a BDD approach (Behaviour-Driven Development) which at the time allowed us to both test hybrid and native mobile apps in all platforms.
+              </li>
+              <li>
+                Rapid Prototyping: used Axure RP, Appery and Pencil.
+              </li>
+              <li>
+                Web Services testing: i.e. SOAP Web Services using SoapUI.
+              </li>
+              <li>
+                Projects: the most important project was a prototype hybrid Android app for Correos (an important client, the equivalent of Royal Mail UK in Spain) to be used in dedicated tablets in post offices to let customers buy most of the services.
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="experience-item">
+          <div class="company">
+            Grupo Coeco
+          </div>
+          <div class="company-logo">
+            company-logo
+          </div>
+          <div class="title">
+            graphic design
+          </div>
+          <div class="from">
+            2010-03
+          </div>
+          <div class="until">
+            2013-03
+          </div>
+          <div class="location">
+            Madrid
+          </div>
+          <div class="tldr">
+            part-time graphic designer during 3 years for a non-technology company
+          </div>
+          <div class="description">
+            <p>
+              While studying in college, I worked mostly as a graphic designer for this kitchen designer firm in Madrid. Designing corporate branding and managing online/offline marketing. I helped as well in the development and maintenance of several websites owned by the parent company. Also, I did office and business IT tasks, and organised training sessions for employees.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section id="education">
+      <h1>where I've studied</h1>
+      <ul>
+        <li class="education-item">
+          <div class="institution">
+            Universidad Politécnica de Madrid
+          </div>
+          <div class="from">
+            2009
+          </div>
+          <div class="until">
+            2014
+          </div>
+          <div class="title">
+            Bachelor's Degree in Computers Engineering
+          </div>
+        </li>
+        <li class="education-item">
+          <div class="institution">
+            HTW Berlin
+          </div>
+          <div class="from">
+            2013
+          </div>
+          <div class="until">
+            2013
+          </div>
+          <div class="title">
+            Degree's Final Project
+          </div>
+          <div class="description">
+            During an Erasums scholarship I developed Secutrak, a native Android app with special tracking features that could be used in a wide range of applications.
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section id="contact">
+      <h1>how you can get in touch with me</h1>
+      <ul>
+        <li id="email">
+          <button onclick="getEmail()">
+            Click here to reveal my e-mail address
+          </button>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jdvivar" target="_blank">Linkedin</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/danielvivar" target="_blank">Facebook</a>
+        </li>
+      </ul>
+    </section>
+    <section id="about">
+      <h1>about this site</h1>
+      <p>
+        I'll explain why I did this site like this
+      </p>
+    </section>
+
+  </main>
   </div>
 </template>
 
