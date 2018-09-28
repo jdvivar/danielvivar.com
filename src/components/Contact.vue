bf9ba904c84063d9a2f8619d9721e5fc03671736
Daniel Vivar <jdvivar@gmail.com>
2018-09-28 21:21:16 +0100

    Adding spinners to contact buttons

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -16,6 +16,10 @@
           v-else
           @click="getEmail()">
           Reveal my e-mail address
+          <font-awesome-icon
+            v-if="loadingEmail"
+            :icon="['fal', 'spinner-third']"
+            spin />
         </button>
       </li>
       <li>
@@ -29,6 +33,10 @@
           v-else
           @click="getPhoneNumber()">
           Reveal my phone number
+          <font-awesome-icon
+            v-if="loadingPhoneNumber"
+            :icon="['fal', 'spinner-third']"
+            spin />
         </button>
       </li>
       <li>
@@ -53,9 +61,9 @@ import LambdaFunctions from '@/services/LambdaFunctions'
 // Icons component
 import { library as Icons } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faMapMarked, faExternalLink } from '@fortawesome/pro-light-svg-icons'
+import { faMapMarked, faExternalLink, faSpinnerThird } from '@fortawesome/pro-light-svg-icons'
 
-Icons.add(faMapMarked, faExternalLink)
+Icons.add(faMapMarked, faExternalLink, faSpinnerThird)
 
 export default {
   name: 'Contact',
@@ -65,15 +73,19 @@ export default {
   data () {
     return {
       email: '',
-      phoneNumber: ''
+      loadingEmail: false,
+      phoneNumber: '',
+      loadingPhoneNumber: false
     }
   },
   methods: {
     async getEmail () {
+      this.loadingEmail = true
       const response = await LambdaFunctions.getEmail()
       this.email = response.data
     },
     async getPhoneNumber () {
+      this.loadingPhoneNumber = true
       const response = await LambdaFunctions.getPhoneNumber()
       this.phoneNumber = response.data
     }
