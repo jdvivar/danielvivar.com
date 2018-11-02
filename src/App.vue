fa40e3d8c236d93ecf9ee5b28be0979225578974
Daniel Vivar <jdvivar@gmail.com>
2018-11-02 14:38:33 +0100

    Refactor styles to import from app main component instead

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,10 @@ export default {
   }
 }
 </script>
-<style>
+
+<style lang="scss">
+@import "@/styles/all.scss";
+
 body {
   margin: 0;
 }
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -114,7 +114,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import "@/styles/all.scss";
-</style>
