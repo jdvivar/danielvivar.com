f4b8e2ef2634b3dad9acae9a127f21d43a29821b
Daniel Vivar <jdvivar@gmail.com>
2021-05-28 09:27:31 +0200

    update number

--- lambda/src/getPhoneNumber.js
+++ lambda/src/getPhoneNumber.js
@@ -1,6 +1,7 @@
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: '+44 7477 122 762',
+    // body: '+44 7477 122 762',
+    body: '+34 657 02 32 59',
   });
 }
