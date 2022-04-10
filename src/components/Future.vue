bbfc150b4b76d6d2abc6d9869f1e6c91d3bf1cad
Daniel Vivar <jdvivar@gmail.com>
2022-04-10 03:22:17 +0200

    feat(Future): add content for print

--- src/components/Future.vue
+++ src/components/Future.vue
@@ -3,14 +3,30 @@
     <h1>what I'm looking for</h1>
     <div class="section-description">
       <p>
-        A new company I can help grow!
+        A company I can help grow!
       </p>
-      <p>
+      <p class="no-print">
         My future company should be a place where <b>learning</b> and sharing knowledge is <b>actively encouraged</b>. I feel specially motivated in projects where we can collaborate into creating <b>delightful user experiences</b> for great products.
       </p>
-      <p>
+      <p class="no-print">
         Other features that thrive me are an <b>open-minded culture</b>, flexibility, a healthy environment, a focused team and companies where technology is nourished from its heart.
       </p>
+      <ul
+        class="print-only"
+        style="list-style: inside '—'"
+      >
+        <li>
+          Sharing and learning encouraged
+        </li>
+        <li>
+          Focus on customer experience
+        </li>
+        <li>
+          A company where technology and their people is at heart
+        </li>
+        <br>
+        <small>(full redaction at website)</small>
+      </ul>
     </div>
   </section>
 </template>
